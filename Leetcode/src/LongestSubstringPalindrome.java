e40e5c96eec6f0019f095b0e1105d7945cc41682
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-08-25 14:19:03 +0530

    initial commit

---
+++ Leetcode/src/LongestSubstringPalindrome.java
@@ -0,0 +1,11 @@
+class palindrome{
+    public String fun(String s){
+        if(s.length() <= 1) return s;
+    }
+}
+
+public class LongestSubstringPalindrome {
+    public static void main(String[] args) {
+
+    }
+}
